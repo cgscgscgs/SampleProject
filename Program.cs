989380c5a5e1ca0f9532c8d311eddade16276590
Celina G Schlecht <156964413+cgscgscgs@users.noreply.github.com>
2025-03-04 16:47:40 -0800

    Adding number to user input, printing it.

--- Program.cs
+++ Program.cs
@@ -1,4 +1,16 @@
-﻿using System;
+﻿/* date             developer              changes
+ * 3/4/2025        Celina                 Created the project
+ * 3/4/2025        Celina                 Asked and read for user input
+ * 3/4/2025        Celina                 Added 999 to the user input, printed the result
+ * 
+ * 
+ * 
+ */
+
+
+
+
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -18,8 +30,13 @@ namespace SampleProject
             // Ask user for a number and read it
             Console.WriteLine("Input a number: ");
             int UserInput = int.Parse(Console.ReadLine());
-            
-            
+
+            // Making another change to Celina_Branch_1
+            // Adding 999 to the user input
+            int result = UserInput + 999;
+
+            // Printing the result
+            Console.WriteLine("The result is: " + result);
         }
     }
 }
